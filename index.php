b0d7285b3c54cff61be4dd3bc529e9fe73664190
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-04-06 10:38:09 -0300

    criado swith de paginas

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 
-  include 'view/index.php';
+  require 'controller/HomeController.php';
+
+  $controller = new HomeController();
+  $pagina = isset($_GET['pagina'])?$_GET['pagina']: 'index';
+
+
+  switch($pagina){
+    case 'index':
+      $controller->index();
+    break;
+    case 'login':
+      $controller->login();
+    break;
+    case 'cadastro':
+      $controller->cadastro();
+    break;
+    case 'ranking':
+      $controller->ranking();
+    break;
+    case 'sobre':
+      $controller->sobre();
+    break;
+    default:
+      echo "página não encontrada";
+  }
 
 
 ?>
