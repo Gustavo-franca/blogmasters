dbe52e51416a1ad703671fd37d9d2cebc2084ca5
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-04-07 20:56:05 -0300

    Redirecionado controle da aplicação para classe App, receber  erros e redirecionando a classe Erro e implementado  autoload com composer

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <?php
 
-  require 'controller/HomeController.php';
+  use App\App;
+  use App\Lib\Erro;
 
-  $controller = new HomeController();
-  $pagina = isset($_GET['pagina'])?$_GET['pagina']: 'index';
+  session_start();
 
+  error_reporting(E_ALL & ~E_NOTICE); // não recebe erros de noticia (como funcões deprecate)
 
-  switch($pagina){
-    case 'index':
-      $controller->index();
-    break;
-    case 'login':
-      $controller->login();
-    break;
-    case 'cadastro':
-      $controller->cadastro();
-    break;
-    case 'ranking':
-      $controller->ranking();
-    break;
-    case 'sobre':
-      $controller->sobre();
-    break;
-    default:
-      echo "página não encontrada";
+  require_once("vendor/autoload.php"); // autoload do composer
+
+  try { // com essa prática todos os erro da aplicação voltaram para aqui caso ocorrer
+    $app = new App(); // esta classe é responsavel po comandar toda aplicação utilizando práticas de um framework.
+    $app->run();
+  }catch(\Exception $e){
+    $oError = new Erro($e);
+    $oError->render(); // envia para a classe erro para mostrar uma página amigavel do Erro ocorrido;
   }
+
+
+
 
 
 ?>
