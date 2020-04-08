4952d18c4cfaa3e92a065babe9c0a82bda788f01
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-04-07 22:09:21 -0300

    Tratamento de Erro da Aplicação

---
+++ App/Lib/Erro.php
@@ -0,0 +1,30 @@
+<?php
+    
+    namespace App\Lib;
+    
+    use Exeption;
+
+    
+    class Erro
+    {
+        private $mensage;
+        private $code;
+
+        public function __contruct($objetoExeption = Exception::class){ // inicializa a classe Exception nativa do PHP
+            $this->code = $objetoExeption->getCode(); // armazena os dados da exeção na váriavel da classe
+            $this->message = $objetoExeption->getMessage();
+        }
+
+        public function render(){
+            $varMessage = $this->message;
+
+            if(file_exists(PATH . "/App/Views/error/".$this->code.".php")){ // verifica se existe uma View com o nome do codigo do Erro
+                require_once PATH . "/App/Views/error/". $this->code.".php";
+            }else{
+                require_once PATH . "/App/Views/error/500.php"; // Página padrão para erros diferentes do Comum;
+            }
+            exit;
+        }
+    }
+
+?>
