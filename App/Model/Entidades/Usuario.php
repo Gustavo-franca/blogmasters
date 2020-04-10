446f88c7d9cb66243277ea4c6f4edb30dcf7d754
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-04-10 19:04:54 -0300

    get e seteres e encapsulamento do método salvar de UsuarioDAO

--- App/Model/Entidades/Usuario.php
+++ App/Model/Entidades/Usuario.php
@@ -5,31 +5,110 @@
     use App\Model\DAO\UsuarioDAO;
 
     class Usuario 
-    {
-        private $fistName;
-        private $lastName;
+    {  
         private $email;
         private $password;
+        private $fistName;
+        private $lastName;
         private $birthDate;
-        private $registrationDate;
+        private $registrationDate; // informação colocada ao inserir  utilizar o método insert
         private $region; //endereço futuramente
-        private $File;
+        private $file;
+        private $DAO;
 
-        
-        
-        
-        public function setNome($nome){
-            $this->nome = $nome;
+        public function __construct(){
+            $this->DAO = new UsuarioDAO;
         }
 
-        public function setEmail ($email){
-            $this->email = $email;
+
+        public function setEmail($email){
+            if(!$this->DAO->verificaEmail($email)){
+                $this->email = $email;
+                return true;
+                //poderia salvar diretamente com risco de não gravar outros dados
+                //tendo que deletar depois 
+            }
+            return false;
         }
 
-        public function setpassword ($password){
-            $this->password = $password;
+     
+
+        public function setPassword($password, $repPassword){
+            if($password == $repPassword){
+                $this->password = $password;
+                return true;
+            }
+            return false;
         }
 
+        public function setFistName($fistName){
+            if($fistName != null){
+                $this->fistName = $fistName;
+                return true;
+            }
+            return false;
+        }
+        public function setLastName($lastName){
+            if($lastName != null){
+                $this->lastName = $lastName;
+                return true;
+            }
+            return false;
+        }
+
+        public function setBirthDate($birthDate){
+            if($birthDate != null){
+                $this->birthDate = $birthDate;
+                return true;
+            }
+            return false;  
+        }
+        
+             
+        public function setRegion($region){
+            if($region != null){//posso validar se essa região existe
+                $this->region = $region;
+                return true;
+            }
+            return false;  
+        }
+
+    
+        public function setFile($file){ // como fazer
+            if($file != null){//posso validar se essa região existe  
+             $this->file = $file;
+                return true;
+            }
+            return false;  
+        }
+        
+        public function getEmail (){
+            return $this->email;
+        }
+        public function getPassword (){
+            return $this->password;
+        }
+        public function getFistName (){
+            return $this->fistName;
+        }
+        public function getLastName (){
+            return $this->lastName;
+        }
+        public function getBirthDate (){
+            return $this->birthDate;
+        }
+        public function getRegion (){
+            return $this->region;
+        }
+        public function getFile (){
+            return $this->file;
+        }
+        public function salvar(){ // como fazer
+            if($this->DAO->salvar($this)){//posso validar se essa região existe  
+                return true;
+            }
+            return false;  
+        }
         
 
 
