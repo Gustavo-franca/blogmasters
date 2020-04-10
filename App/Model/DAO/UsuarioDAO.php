b48db12630b54e0f554f5f26d86886fc66500d0f
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-04-10 18:58:51 -0300

    crisçssof métofo salvar e correção do método velidaEmail

--- App/Model/DAO/UsuarioDAO.php
+++ App/Model/DAO/UsuarioDAO.php
@@ -3,6 +3,7 @@
     namespace App\Model\DAO;
 
     use App\Model\Entidades\Usuario;
+    use DateTime;
 
     class UsuarioDAO extends BaseDAO 
     {
@@ -11,7 +12,7 @@
             try{
                $query =  $this->select(
                    
-                "SELECT FROM user WHERE email = 'email' "
+                "SELECT email FROM user WHERE email = '$email' "
 
                 );
                 return $query->fetch();
@@ -19,6 +20,35 @@
                 throw \Exception("Erro de acesso aos Dados", 500);
             }
 
+        }
+
+        public function salvar($usuario){
+            $idAttachment = 1;
+            $registrationDate = '2020-04-10 14:30:30';
+            $fistName = $usuario->getFistName();
+            $lastName = $usuario->getLastName();
+            $email = $usuario->getEmail();
+            $password = $usuario->getPassword();
+            $birthDate = $usuario->getBirthDate();
+            $idRegion = $usuario->getRegion();
+            
+           
+           
+            return $this->insert(
+                "User",
+                " :fistName, :lastName, :email, :password, :birthDate, :registrationDate, :idAttachment, :idRegion",
+                [
+                    ':fistName'=>$fistName,
+                    ':lastName'=>$lastName,
+                    ':email'=>$email,
+                    ':password'=>$password,
+                    ':birthDate'=>$birthDate,
+                    ':registrationDate'=>$registrationDate,
+                    ':idAttachment'=>$idAttachment,
+                    ':idRegion'=>$idRegion
+                ]
+            );
+          
         }
 
 
