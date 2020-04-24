ba0789e77d8e5310e6a5a29ebc3b0a2c06c8fcca
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-04-24 10:33:34 -0300

    correções de sintaxe

--- App/Lib/Session.php
+++ App/Lib/Session.php
@@ -3,11 +3,23 @@
     namespace App\Lib;
 
     class Session {
-
-        public static function notshownav(){
-            $_SESSION['nav'] = true;
-        }
         
+
+        public static function setVar($name,$value){
+            $_SESSION[$name] = $value;
+        }
+        public static function getVar($name){
+            return ($_SESSION[$name])?$_SESSION[$name]:"";
+        }
+        public static function showNav($bool){
+            $_SESSION['nav'] = $bool;
+        }
+        public static function getshownav(){
+            return (isset($_SESSION['nav']))?$_SESSION['nav']:true;
+        }
+        public static function clearShowNav(){
+            unset( $_SESSION['nav']);
+        }
         public static function setMessage($messagem){
             $_SESSION['message'] = $messagem;
         }
@@ -17,7 +29,7 @@
         }
 
         public static function getMessage(){
-            return ($_SESSION['message'])?$_SECTION['message']:"";
+            return ($_SESSION['message'])?$_SESSION['message']:"";
         }
 
         public static function setForm($form){
@@ -28,14 +40,11 @@
             unset($_SESSION['form']);
         }
         public static function getValorForm($key){
-            return ($_SECTION['form'][$key])?$_SECTION['form'][$key]:"";
+            return ($_SESSION['form'][$key])?$_SESSION['form'][$key]:"";
         }
         public static function getForm($key){
-            return ($_SECTION['form'])?$_SECTION['form']:"";
+            return ($_SESSION['form'])?$_SESSION['form']:"";
         }
-
-
-
     }
 
 ?>
