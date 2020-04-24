5afa5f4a8efde73ff64a1dcedd5357efc22d8c92
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-04-24 10:52:32 -0300

    Correção de html

--- App/Views/layouts/footer.php
+++ App/Views/layouts/footer.php
@@ -1,11 +1,19 @@
-        <footer id="footer"> 
-            <div id="footer-text">
-                <p>&copy;Gustavo Cândido França</p>
-                <p>All rigths Reserved@2019</p>
-            </div>
-        </footer>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="<?php echo 'http://'.APP_HOST?>Public/js/main.js">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 bg-danger">
+            <footer id="footer"> 
+                
+                
+                <div class="text-center"id="footer-text" style="color:white;">
+                    <p class="mb-0 pt-1">&copy;Gustavo Cândido França</p>
+                    <p>All rigths Reserved@2019</p>
+                </div>
+            </footer>
+        </div>    
+    </div>
+</div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="<?php echo 'http://'.APP_HOST?>Public/js/main.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
        
